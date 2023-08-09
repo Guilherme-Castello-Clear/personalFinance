c94cef3fdc081d70a5cf6b2e1e9fe5a6dc72e92f
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-08-08 21:04:41 -0300

    Feat: Creating daily cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
-
+import api from "../../services/api";
 import { Background } from "./styles";
 import Header from "../../components/Header";
+import { format } from "date-fns";
 function Home(){
 
+    const [listBalance, setListBalance] = useState([])
+    const [dateMovements, setDateMovements] = useState(new Date())
     const { signOut, user } = useContext(AuthContext)
+
+    useEffect(() => {
+        let isActive = true;
+        async function getMovements(){
+            let dateFormated = format(dateMovements, 'dd/mm/yyyy')
+            const balance = await api.get('/balance', {
+                params:{
+                    date: dateFormated
+                }
+            })
+            if(isActive){
+                setListBalance(balance.data)
+            }
+        }
+        getMovements()
+
+        return () => isActive = false;
+    }, [])
 
     return(
         <Background>
