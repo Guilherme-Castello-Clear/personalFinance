b327353a8cac87782e0a7c507e3bcccef187584b
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-08-08 21:41:08 -0300

    Feat: Creating daily cards²

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,21 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 import api from "../../services/api";
-import { Background } from "./styles";
+import { Background, ListBalance } from "./styles";
 import Header from "../../components/Header";
 import { format } from "date-fns";
+import { useIsFocused } from "@react-navigation/native";
+import BalanceItem from "../../components/BalanceItem";
 function Home(){
 
+    const isFocus = useIsFocused();
     const [listBalance, setListBalance] = useState([])
     const [dateMovements, setDateMovements] = useState(new Date())
     const { signOut, user } = useContext(AuthContext)
 
     useEffect(() => {
         let isActive = true;
+        
         async function getMovements(){
             let dateFormated = format(dateMovements, 'dd/mm/yyyy')
             const balance = await api.get('/balance', {
@@ -21,17 +24,25 @@ function Home(){
                 }
             })
             if(isActive){
+                console.log(balance.data)
                 setListBalance(balance.data)
             }
         }
         getMovements()
 
         return () => isActive = false;
-    }, [])
+    }, [isFocus])
 
     return(
         <Background>
             <Header title="Minhas Movimentações"/>
+            <ListBalance
+                data={listBalance}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={ item => item.tag }
+                renderItem={ ({item}) => (<BalanceItem data={item}/>)}
+            />
         </Background>
     )
 }
