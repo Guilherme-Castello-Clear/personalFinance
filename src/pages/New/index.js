37eb98381c74257a092f309628426dcc45671f29
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-08-09 15:55:59 -0300

    Feat: Created system to save new registry

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { format } from 'date-fns';
 
+import api from '../../services/api';
 import RegisterTypes from '../../components/RegisterTypes';
 import { Background, Input, SubmitButton, SubmitText } from './styles';
 import Header from '../../components/Header'
@@ -10,6 +12,43 @@ export default function New(){
   const [labelInput, setLabelInput] = useState('')
   const [valueInput, setValueInput] = useState('')
   const [type, setType] = useState('receita')
+
+  function handleSubmit(){
+    Keyboard.dismiss();
+    if(isNaN(parseFloat(valueInput) || labelInput == '' || type === null)){
+      Alert('Preencha todos os campos')
+      return
+    }
+
+    Alert.alert(
+      'Confirmando dados',
+      `Tipo: ${type} - Valor: ${parseFloat(valueInput)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handleAdd(){
+    Keyboard.dismiss()
+
+    await api.post('./receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/mm/yyyy')
+    })
+
+    setLabelInput('')
+    setValueInput('')
+  }
 
   return(
     
@@ -23,7 +62,7 @@ export default function New(){
           <Input value={valueInput} onChangeText={text => setValueInput(text)} placeholder="Valor" keyboardType="numeric"/>
           <RegisterTypes type={type} sendTypeChanged={item => setType(item)}/>
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </SafeAreaView>
